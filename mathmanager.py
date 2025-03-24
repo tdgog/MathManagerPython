f1183b741deba15758859e0dc0964dbb1bac148a
tdgog <tdgog321@gmail.com>
2025-03-24 12:34:31 +0000

    Add degree classification

--- mathmanager.py
+++ mathmanager.py
@@ -1,4 +1,7 @@
-class mathmanager:
+from sympy.strategies.core import switch
+
+
+class MathManager:
 
 	def add(self, a, b):
 			return a+b
@@ -8,3 +11,39 @@ class mathmanager:
 
 	def multiply(self, a, b):
 			return a*b
+
+	def calculate_monthly_interest(self, money: float, years: int):
+		pass
+
+	def calculate_tax_paid(self, income: float) -> float:
+		taxable = income - 12570
+
+		# No taxable income, therefore no taxes paid
+		if taxable <= 0:
+			return 0
+
+		# If they make less than the threshold for 40%, tax the full income at 20%
+		if taxable <= 50270:
+			return taxable * 0.2
+
+		# If they make less than the threshold for 45%, tax the first 50270 at 20% and the rest at 40%
+		if taxable <= 125140:
+			return 50270 * 0.2 + (taxable - 50270) * 0.4
+
+		# If they make more than the threshold for 45%, tax the first 50270 at 20%, the next 74870 at 40%,
+		# and the rest at 45%
+		return 50270 * 0.2 + (125140 - 50270) * 0.4 + (taxable - 50270 - 125140) * 0.45
+
+	def calculate_degree_classification(self, marks: list[int]) -> str:
+		marks.remove(min(marks))
+		average_mark = sum(marks) / len(marks)
+
+		if average_mark < 40:
+			return "fail"
+		elif average_mark < 50:
+			return "3rd"
+		elif average_mark < 60:
+			return "2:2"
+		elif average_mark < 70:
+			return "2:1"
+		return "1st"
--- mathmanagertest.py
+++ mathmanagertest.py
@@ -1,15 +1,23 @@
 import unittest
-from mathmanager import mathmanager
+from mathmanager import MathManager
 
 class mathmanagertest(unittest.TestCase):
 	def testAdd(self):
-		math = mathmanager()
+		math = MathManager()
 		self.assertEqual(math.add(0, 3), 3)
 
 	def testSubtract(self):
-		math = mathmanager()
+		math = MathManager()
 		self.assertEqual(math.subtract(0, 3), -3)
 
 	def testMultiply(self):
-		math = mathmanager()
+		math = MathManager()
 		self.assertEqual(math.multiply(0, 3), 0)
+
+	def test_calculate_degree_classification(self):
+		math = MathManager()
+		self.assertEqual(math.calculate_degree_classification([40, 40, 40, 10, 10]), "fail")
+		self.assertEqual(math.calculate_degree_classification([40, 40, 40, 40, 10]), "3rd")
+		self.assertEqual(math.calculate_degree_classification([100, 35, 60, 45, 62]), "2:1")
+		self.assertEqual(math.calculate_degree_classification([80, 35, 50, 45, 62]), "2:2")
+		self.assertEqual(math.calculate_degree_classification([0, 70, 70, 70, 70]), "1st")
